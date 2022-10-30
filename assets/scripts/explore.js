7542893cc5489b5e208a934d39525434642a327b
RichardHeriawan <rheriawan@ucsd.edu>
2022-10-30 01:22:37 -0700

    update files

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,56 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const playButton = document.querySelector('#explore button'); 
+  const textInput = document.getElementById("text-to-speak"); 
+  const smileyface = document.querySelector('#explore img');
+  const select = document.getElementById("voice-select");
+
+  let voices;
+  let currentVoice; 
+
+  const populateVoices = () => {
+    const availableVoices = speechSynthesis.getVoices();
+    select.innerHTML = '';
+
+    availableVoices.forEach(voice => {
+      const option = document.createElement('option');
+      let optionText = `${voice.name} (${voice.lang})`;
+      if (voice.default) {
+        optionText += ' [default]';
+        if (typeof currentVoice === 'undefined') {
+          currentVoice = voice;
+          option.selected = true;
+        }
+      }
+      if (currentVoice === voice) {
+        option.selected = true;
+      }
+      option.textContent = optionText;
+      select.appendChild(option);
+    });
+    voices = availableVoices;
+  };
+
+  populateVoices();
+  speechSynthesis.onvoiceschanged = populateVoices;
+
+  select.addEventListener('change', event => {
+    const selectedIndex = event.target.selectedIndex;
+    currentVoice = voices[selectedIndex];
+  });
+
+  playButton.addEventListener('click', () => {
+    playText(textInput.value); 
+  })
+
+  function playText(text) { 
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = currentVoice;
+    smileyface.src = "assets/images/smiling-open.png";
+    utterance.addEventListener('end', () => {
+      smileyface.src = "assets/images/smiling.png";
+    })
+    speechSynthesis.speak(utterance); 
+  }
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,54 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const jsConfetti = new JSConfetti(); 
+  const hornimages = document.querySelector('#expose img');
+  const hornselect = document.querySelector('#horn-select');
+  const volume = document.querySelector('#volume');
+  const button = document.querySelector('#expose button');
+
+  hornselect.addEventListener('change', () => {
+    if(hornselect.value == "air-horn") {
+      hornimages.src = 'assets/images/air-horn.svg';
+    } else if(hornselect.value === "car-horn") {
+      hornimages.src = 'assets/images/car-horn.svg';
+    } else if(hornselect.value === "party-horn") {
+      hornimages.src = 'assets/images/party-horn.svg';
+    } else {
+        return;
+    }
+    });
+
+  volume.addEventListener('change', () => {
+    if(volume.value == 0) {
+      volume.src = 'assets/icons/volume-level-0.svg';
+    } else if(volume.value > 0  && volume.value < 33) {
+      volume.querySelector('#volume-controls img').src = 'assets/icons/volume-level-1.svg';
+    } else if(volume.value > 32 && volume.value < 67) {
+      volume.querySelector('#volume-controls img').src = 'assets/icons/volume-level-2.svg';
+    } else if(volume.value > 66 && volume.value < 100) {
+      volume.querySelector('#volume-controls img').src = 'assets/icons/volume-level-3.svg';
+    } else {
+      return;
+    }
+    });
+  
+    button.addEventListener('click', () => {
+      if(hornselect.value == "air-horn") {
+        var airhorn = new Audio("assets/audio/air-horn.mp3");
+        airhorn.volume = parseFloat(volume.value)/100; 
+        airhorn.play();
+      } else if(hornselect.value === "car-horn") {
+        var carhorn = new Audio("assets/audio/car-horn.mp3");
+        carhorn.volume = parseFloat(volume.value)/100; 
+        carhorn.play();
+      } else if(hornselect.value === "party-horn") {
+        jsConfetti.addConfetti();
+        var partyhorn = new Audio("assets/audio/party-horn.mp3");
+        partyhorn.volume = parseFloat(volume.value)/100; 
+        partyhorn.play();
+      } else {
+        return;
+      }
+    });
 }
